ca838445975d1e6749c2fb25e25d68aae5abc4b8
gct <gct@yonyou.com>
2016-11-17 09:06:46 +0800

    tt

---
+++ test_hybrid/js/index.js
@@ -0,0 +1,18 @@
+﻿
+
+summerready = function(){
+    // here is your code...	
+    var top = $summer.offset($summer.byId('header')).h;
+	var bottom = $summer.offset($summer.byId('footer')).h;
+    summer.openFrame({
+        id: 'main',
+        url: 'html/main.html',
+        bounces: true,
+        position: {
+            top: top,
+			bottom: bottom,
+            left: 0,
+            right: 0
+        }
+    });
+}
---
+++ test_hybrid/js/main.js
@@ -0,0 +1,4 @@
+//here is your code...
+summerready = function () {
+	$summer.byId("content").innerHTML += "<h3 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h3><h3 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h3>";
+};
