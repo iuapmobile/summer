3f440dc7837620b83f9b5c182331e9bbac7c882c
gct <gct@yonyou.com>
2017-03-07 13:35:30 +0800

    tt

--- dist/js/summer.js
+++ dist/js/summer.js
@@ -382,6 +382,7 @@
 					summerinit();//框架调用
 				else if(typeof summerInit == "function")
 					summerInit();//框架调用
+				summer.trigger("init");
 				
                 //1、先通过cdv来获取页面参数
                 summer.winParam(function(ret){
@@ -419,11 +420,13 @@
                         summerready();
                     else if(typeof summerReady == "function")
                         summerReady();  
+					summer.trigger("ready");
 					
 					if(typeof aftershowwin == "function")
                         aftershowwin();
                     else if(typeof afterShowWin == "function")
-                        afterShowWin();  
+                        afterShowWin();
+					summer.trigger("aftershowwin");
                 });         
             }, false);
 
@@ -469,8 +472,54 @@
 		}
 		return true;
 	};
-   
 	w.$summer.require = w.summer.require;
+	
+	var EventMgr = function() {
+        this._events = {};
+    }
+    EventMgr.prototype.on = function(evtName, handler) {
+        if (this._events[evtName] == null) {
+            this._events[evtName] = [];
+        }
+        this._events[evtName].push(handler);
+    }
+    EventMgr.prototype.off = function(evtName, handler) {
+        var handlers = this._events[evtName];
+        if (typeof handler == "undefined") {
+            delete handlers;
+        } else {
+            var index = -1;
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                if (handler == handlers[i]) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index > 0)
+                handlers.remove(index);
+        }
+    }
+    EventMgr.prototype.trigger = function(evtName, sender, args) {
+        try{
+            var handlers = this._events[evtName];
+			if(!handlers) return;
+            var handler;
+            args = args || {};
+            for (var i=0,len=handlers.length; i < len; i++) {
+                handler = handlers[i];
+                handler(sender, args);
+            }
+        }catch(e){
+            alert(e);
+        }
+    }
+	var _ems = new EventMgr();
+	w.summer.on = function(eName, fn){
+		_ems.on(eName, fn);
+	}
+	w.summer.trigger = function(eName){
+		_ems.trigger(eName);
+	}
 })(window);
 
 
