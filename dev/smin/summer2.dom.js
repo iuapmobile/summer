32c5cd0a9a742b605ebd42fc92a1c951363068d0
xingychb <xingychb@yonnyou.com>
2017-07-24 15:56:03 +0800

    1

--- dev/smin/summer2.dom.js
+++ dev/smin/summer2.dom.js
@@ -1,3 +1,28 @@
+(function(global, factory){
+    if ( typeof module === "object" && typeof module.exports === "object" ) {
+        module.exports = global.document ?
+            factory( global, true ) :
+            function( w ) {
+                if ( !w.document ) {
+                    throw new Error( "jQuery requires a window with a document" );
+                }
+                return factory( w );
+            };
+    } else {
+        factory( global );
+    }
+}(window,function(window,noGlobal){
+    var $s = {};
+    var s = {$:$s};
+    if ( typeof define === "function" && define.amd ) {
+        define( "summer", [], function() {
+            return s;
+        });
+    }
+    window.$summer = $s;
+    window.summer = s;
+    return s;
+}));
 //HTML DOM API by gct
 ;(function(window){
     var u = window.$summer || {};
