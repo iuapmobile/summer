ffec79f6d5a2d4767bbca230f12b647a314ffe1c
Sfl <942368681@qq.com>
2018-02-08 12:31:07 +0800

    封装 im API

--- dev/js/summer.im.js
+++ dev/js/summer.im.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
-        module.exports = global.document ?
-            factory(global, true) :
-            function (w) {
+        module.exports = global.document
+            ? factory(global, true)
+            : function (w) {
                 if (!w.document) {
                     throw new Error("jQuery requires a window with a document");
                 }
@@ -15,8 +15,7 @@
     var i = {};
     window.im = i;
     return im;
-}))
-+ function (w, i, s) {
+})) + function (w, i, s) {
     if (!i) {
         i = {};
         w.im = i;
@@ -33,6 +32,174 @@
             "params": json
         };
         params["method"] = "YYIM.logout";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.createGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.createGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getChatGroups = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getChatGroups";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.chat = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.chat";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.registerMessageObserver = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.registerMessageObserver";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getRecentContacters = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getRecentContacters";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.fetchAccountMessages = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.fetchAccountMessages";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.openFaceToFace = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.openFaceToFace";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getChatGroupMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getChatGroupMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.groupKickMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.groupKickMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.forwardMessage = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.forwardMessage";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.groupAddMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.groupAddMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.updateUserInfo = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.updateUserInfo";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.fetchMessages = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.fetchMessages";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setStickTop = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setStickTop";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setNoDisturb = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setNoDisturb";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteChat = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteChat";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteMessage = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteMessage";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.joinGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.joinGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setSilenceMode = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setSilenceMode";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.isSilenceMode = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.isSilenceMode";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.searchByKey = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.searchByKey";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.updateMessageReaded = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.updateMessageReaded";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.chatSearch = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.chatSearch";
         s.callServiceEx(params, successFn, errFn);
     };
 }(window, im, summer);
--- dist/js/summer.js
+++ dist/js/summer.js
@@ -1841,7 +1841,15 @@
         }
     };//s.service end
     //
-    s.callServiceEx = function (json) {
+    s.callServiceEx = function (json, successFn, errFn) {
+        if(successFn){
+            json.params["callback"] = successFn;
+            s.UMService._callbackProxy(json.params, "callback");
+        }
+        if(errFn){
+            json.params["error"] = errFn;
+            s.UMService._callbackProxy(json.params, "error");
+        }
         return s.callCordova('summer-plugin-service.XService', 'callSync', json, null, null);
     };
 
@@ -2627,9 +2635,9 @@
 }(window, emm, summer);
 (function (global, factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
-        module.exports = global.document ?
-            factory(global, true) :
-            function (w) {
+        module.exports = global.document
+            ? factory(global, true)
+            : function (w) {
                 if (!w.document) {
                     throw new Error("jQuery requires a window with a document");
                 }
@@ -2639,23 +2647,194 @@
         factory(global);
     }
 }(window, function (window, noGlobal) {
-    var e = {};
-    window.im = e;
+    var i = {};
+    window.im = i;
     return im;
-}))
-+ function (w, e, s) {
-    if (!e) {
-        e = {};
-        w.im = e;
+})) + function (w, i, s) {
+    if (!i) {
+        i = {};
+        w.im = i;
     }
-    e.login = function (json, successFn, errFn) {
-        json["callback"] = successFn;
-        json["error"] = errFn;
-        s.callService("YYIM.login", json, false);
+    i.login = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.login";
+        s.callServiceEx(params, successFn, errFn);
     };
-    e.logout = function (json, successFn, errFn) {
-        json["callback"]=successFn;
-        json["error"]=errFn;
-        s.callService("YYIM.logout", json, false);
+    i.logout = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.logout";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.createGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.createGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getChatGroups = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getChatGroups";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.chat = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.chat";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.registerMessageObserver = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.registerMessageObserver";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getRecentContacters = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getRecentContacters";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.fetchAccountMessages = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.fetchAccountMessages";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.openFaceToFace = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.openFaceToFace";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.getChatGroupMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.getChatGroupMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.groupKickMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.groupKickMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.forwardMessage = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.forwardMessage";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.groupAddMember = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.groupAddMember";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.updateUserInfo = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.updateUserInfo";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.fetchMessages = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.fetchMessages";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setStickTop = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setStickTop";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setNoDisturb = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setNoDisturb";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteChat = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteChat";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.deleteMessage = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.deleteMessage";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.joinGroup = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.joinGroup";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.setSilenceMode = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.setSilenceMode";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.isSilenceMode = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.isSilenceMode";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.searchByKey = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.searchByKey";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.updateMessageReaded = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.updateMessageReaded";
+        s.callServiceEx(params, successFn, errFn);
+    };
+    i.chatSearch = function (json, successFn, errFn) {
+        var params = {
+            "params": json
+        };
+        params["method"] = "YYIM.chatSearch";
+        s.callServiceEx(params, successFn, errFn);
     };
 }(window, im, summer);
